2fe9f12f9a4cdef2fedb09b0db1cc4ff378c694e
sugiyamaleaf <iu-lav.f@docomo.ne.jp>
2025-09-10 23:09:29 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,20 @@
+//定数の宣言
+let weight;
+
+//値の代入
+weight=68
+
+//定数の宣言
+let height;
+
+//値の代入
+height=1.7
+
+//定数の宣言
+let bmi;
+
+//値の代入
+mbi=weight/(height*height)
+
+//コンソールへの出力
+console.log(68/(1.7*1.7));
